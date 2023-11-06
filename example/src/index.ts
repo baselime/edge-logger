024ec5d84e30adb9e2411dba33e774bed4694a67
Ankcorn <thomasankcorn@gmail.com>
2023-11-06 13:00:31 +0000

    add everything

--- example/src/index.ts
+++ example/src/index.ts
@@ -11,70 +11,81 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-import { instrument, ResolveConfigFn } from '@microlabs/otel-cf-workers'
-import { context, trace } from '@opentelemetry/api'
-import { BaselimeLogger } from '../../dist/index'
+import { instrument, ResolveConfigFn } from "@microlabs/otel-cf-workers";
+import { context, trace } from "@opentelemetry/api";
+import { BaselimeLogger } from "../../dist/index";
 export interface Env {
 	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	MY_QUEUE: Queue
-	BASELIME_KEY: string
+	MY_QUEUE: Queue;
+	BASELIME_KEY: string;
 }
 
 const handler = {
 	// Our fetch handler is invoked on a HTTP request: we can send a message to a queue
 	// during (or after) a request.
 	// https://developers.cloudflare.com/queues/platform/javascript-apis/#producer
-	async fetch(req: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+	async fetch(
+		req: Request,
+		env: Env,
+		ctx: ExecutionContext,
+	): Promise<Response> {
 		const logger = new BaselimeLogger({
 			ctx,
 			apiKey: env.BASELIME_KEY,
-			service: 'my-worker',
-			dataset: 'cloudflare',
-			namespace: 'fetch',
-			requestId: req.headers.get('cf-ray'),
-		})
+			service: "my-worker",
+			dataset: "cloudflare",
+			namespace: "fetch",
+			requestId: req.headers.get("cf-ray"),
+		});
 
-		logger.info('Hello world', { cfRay: req.headers.get('cf-ray'), foo: 'bar' })
+		logger.info("Hello world", {
+			cfRay: req.headers.get("cf-ray"),
+			foo: "bar",
+		});
 		// To send a message on a queue, we need to create the queue first
 		// https://developers.cloudflare.com/queues/get-started/#3-create-a-queue
 		await env.MY_QUEUE.send({
 			url: req.url,
 			method: req.method,
 			headers: Object.fromEntries(req.headers),
-		})
+		});
 
-		ctx.waitUntil(logger.flush())
-		return new Response('Sent message to the queue')
+		ctx.waitUntil(logger.flush());
+		return new Response("Sent message to the queue");
 	},
 	// The queue handler is invoked when a batch of messages is ready to be delivered
 	// https://developers.cloudflare.com/queues/platform/javascript-apis/#messagebatch
-	async queue(batch: MessageBatch<Error>, env: Env, ctx: ExecutionContext): Promise<void> {
+	async queue(
+		batch: MessageBatch<Error>,
+		env: Env,
+		ctx: ExecutionContext,
+	): Promise<void> {
 		const logger = new BaselimeLogger({
 			ctx,
 			apiKey: env.BASELIME_KEY,
-			service: 'my-worker',
-			dataset: 'cloudflare',
-			namespace: 'queue',
-		})
+			service: "my-worker",
+			dataset: "cloudflare",
+			namespace: "queue",
+		});
 		// A queue consumer can make requests to other endpoints on the Internet,
 		// write to R2 object storage, query a D1 Database, and much more.
 		for (let message of batch.messages) {
 			// Process each message (we'll just log these)
-			logger.info('message processed', { queueInput: message })
-			logger.info('Done')
+			logger.info("message processed", { queueInput: message });
+			logger.info("Done");
 		}
-		ctx.waitUntil(logger.flush())
+		ctx.waitUntil(logger.flush());
 	},
-}
+};
 
 const config: ResolveConfigFn = (env: Env, _trigger) => {
 	return {
 		exporter: {
-			url: 'https://otel.baselime.io/v1',
-			headers: { 'x-api-key': env.BASELIME_KEY },
+			url: "https://otel.baselime.io/v1",
+			headers: { "x-api-key": env.BASELIME_KEY },
 		},
-		service: { name: 'my-worker' },
-	}
-}
+		service: { name: "my-worker" },
+	};
+};
 
-export default instrument(handler, config)
+export default instrument(handler, config);
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { ExecutionContext } from '@cloudflare/workers-types'
+import { ExecutionContext } from "@cloudflare/workers-types";
 
 export type BaselimeLog = {
-	message: string
-	error?: string
-	requestId: string
-	level: string
-	data: any
-}
+	message: string;
+	error?: string;
+	requestId: string;
+	level: string;
+	data: any;
+};
 
-let tracingApiPromise: Promise<typeof import('@opentelemetry/api') | null>
+let tracingApiPromise: Promise<typeof import("@opentelemetry/api") | null>;
 
 try {
 	/**
@@ -16,91 +16,101 @@ try {
 	 * It can't be provided by this library as the version needs to match the opentelemetry version
 	 * provided by the user
 	 */
-	tracingApiPromise = import('@opentelemetry/api')
+	tracingApiPromise = import("@opentelemetry/api");
 } catch (_) {
-	tracingApiPromise = Promise.resolve(null)
+	tracingApiPromise = Promise.resolve(null);
 }
 
 export type BaselimeLoggerArgs = {
-	ctx: ExecutionContext
-	apiKey: string
-	dataset?: string
-	service?: string
-	namespace?: string
-	baselimeUrl?: string
-	flushAfterMs?: number
-	flushAfterLogs?: number
-	requestId?: string | null
-	isLocalDev?: boolean
-}
+	ctx: ExecutionContext;
+	apiKey: string;
+	dataset?: string;
+	service?: string;
+	namespace?: string;
+	baselimeUrl?: string;
+	flushAfterMs?: number;
+	flushAfterLogs?: number;
+	requestId?: string | null;
+	isLocalDev?: boolean;
+};
 
 export class BaselimeLogger {
-	private readonly ctx: ExecutionContext
-	private readonly apiKey: string
-	private readonly dataset: string
-	private readonly service: string
-	private readonly namespace: string
-	private readonly logs: BaselimeLog[] = []
-	private readonly requestId: string
+	private readonly ctx: ExecutionContext;
+	private readonly apiKey: string;
+	private readonly dataset: string;
+	private readonly service: string;
+	private readonly namespace: string;
+	private readonly logs: BaselimeLog[] = [];
+	private readonly requestId: string;
 	// flushTimeout is a timeout set by setTimeout() to flush the logs after a certain amount of time
-	private flushTimeout: NodeJS.Timeout | null = null
-	private flushPromise: Promise<any> | null = null
-	private flushAfterMs: number
-	private flushAfterLogs: number
-	private baselimeUrl: string
-	private isLocalDev: boolean
+	private flushTimeout: NodeJS.Timeout | null = null;
+	private flushPromise: Promise<any> | null = null;
+	private flushAfterMs: number;
+	private flushAfterLogs: number;
+	private baselimeUrl: string;
+	private isLocalDev: boolean;
 
 	constructor(args: BaselimeLoggerArgs) {
-		this.ctx = args.ctx
-		this.apiKey = args.apiKey
-		this.dataset = args.dataset
-		this.service = args.service
-		this.namespace = args.namespace
-		this.flushAfterMs = args.flushAfterMs ?? 10000
-		this.flushAfterLogs = args.flushAfterLogs ?? 100
-		this.baselimeUrl = args.baselimeUrl ?? 'https://events.baselime.io/v1'
+		this.ctx = args.ctx;
+		this.apiKey = args.apiKey;
+		this.dataset = args.dataset;
+		this.service = args.service;
+		this.namespace = args.namespace;
+		this.flushAfterMs = args.flushAfterMs ?? 10000;
+		this.flushAfterLogs = args.flushAfterLogs ?? 100;
+		this.baselimeUrl = args.baselimeUrl ?? "https://events.baselime.io/v1";
 		if (args.requestId) {
-			this.requestId = args.requestId
+			this.requestId = args.requestId;
 		} else {
-			this.requestId = crypto.randomUUID()
+			this.requestId = crypto.randomUUID();
 		}
-		this.isLocalDev = args.isLocalDev
+		this.isLocalDev = args.isLocalDev;
 	}
 
 	private async _log(message: string, level: string, data?: any) {
-		const tracingApi = await tracingApiPromise
-		let traceId: string | undefined
+		const tracingApi = await tracingApiPromise;
+		let traceId: string | undefined;
 		if (tracingApi) {
-			const span = tracingApi.trace.getActiveSpan()
-			traceId = span?.spanContext().traceId
+			const span = tracingApi.trace.getActiveSpan();
+			traceId = span?.spanContext().traceId;
 		}
 
 		if (this.isLocalDev) {
 			const colors = {
-				info: '\x1b[32m',
-				warning: '${colors[log.level]',
-				error: '\x1b[31m',
-				debug: '\x1b[35m',
-			}
+				info: "\x1b[32m",
+				warning: "${colors[log.level]",
+				error: "\x1b[31m",
+				debug: "\x1b[35m",
+			};
 
-			const grey = `\x1b[90m`
-			const white = `\x1b[0m`
-			console.log(`${colors[level]}${level}${grey} - ${this.requestId} - ${white}${message}`)
+			const grey = `\x1b[90m`;
+			const white = `\x1b[0m`;
+			console.log(
+				`${colors[level]}${level}${grey} - ${this.requestId} - ${white}${message}`,
+			);
 			if (data) {
-				console.log(`${grey} ${JSON.stringify(data, null, 2)}`)
+				console.log(`${grey} ${JSON.stringify(data, null, 2)}`);
 			}
-			return
+			return;
 		}
 		/**
 		 * If no API key or context, we can't send logs so log them to sdtout
 		 */
 		if (!this.apiKey || !this.ctx) {
-			console.log(JSON.stringify({ message, level, ...data, requestId: this.requestId, traceId: data?.traceId }))
+			console.log(
+				JSON.stringify({
+					message,
+					level,
+					...data,
+					requestId: this.requestId,
+					traceId: data?.traceId,
+				}),
+			);
 		}
 
 		if (data && data.level) {
-			level = data.level
-			delete data.level
+			level = data.level;
+			delete data.level;
 		}
 
 		const log: BaselimeLog = {
@@ -109,19 +119,19 @@ export class BaselimeLogger {
 			traceId,
 			requestId: this.requestId,
 			...data,
-		}
+		};
 
-		this.logs.push(log)
+		this.logs.push(log);
 
 		if (this.logs.length >= this.flushAfterLogs) {
 			// Reset scheduled if there is one
 			if (this.flushTimeout) {
-				this.scheduleFlush(this.flushAfterMs, true)
+				this.scheduleFlush(this.flushAfterMs, true);
 			}
-			this.ctx.waitUntil(this.flush({ skipIfInProgress: true }))
+			this.ctx.waitUntil(this.flush({ skipIfInProgress: true }));
 		} else {
 			// Always schedule a flush (if there isn't one already)
-			this.scheduleFlush(this.flushAfterMs)
+			this.scheduleFlush(this.flushAfterMs);
 		}
 	}
 
@@ -131,86 +141,92 @@ export class BaselimeLogger {
 	 */
 	scheduleFlush(timeout: number, reset = false) {
 		if (reset && this.flushTimeout) {
-			clearTimeout(this.flushTimeout)
-			this.flushTimeout = null
+			clearTimeout(this.flushTimeout);
+			this.flushTimeout = null;
 		}
 
 		if (!this.flushTimeout && !this.flushPromise) {
 			this.flushTimeout = setTimeout(() => {
 				const doFlush = async () => {
-					this.flush({ skipIfInProgress: true })
-					this.flushTimeout = null
-				}
-				this.ctx.waitUntil(doFlush())
-			}, timeout)
+					this.flush({ skipIfInProgress: true });
+					this.flushTimeout = null;
+				};
+				this.ctx.waitUntil(doFlush());
+			}, timeout);
 		}
 	}
 
-	async flush({ skipIfInProgress = false }: { skipIfInProgress?: boolean } = {}) {
-		if (skipIfInProgress && this.flushPromise) return
+	async flush({
+		skipIfInProgress = false,
+	}: { skipIfInProgress?: boolean } = {}) {
+		if (skipIfInProgress && this.flushPromise) return;
 
 		const doFlush = async () => {
-			if (this.logs.length === 0) return // Nothing to do
+			if (this.logs.length === 0) return; // Nothing to do
 
-			const logsCount = this.logs.length
-			const logsBody = JSON.stringify(this.logs)
+			const logsCount = this.logs.length;
+			const logsBody = JSON.stringify(this.logs);
 
 			try {
 				const res = await fetch(`${this.baselimeUrl}/${this.dataset}`, {
-					method: 'POST',
+					method: "POST",
 					headers: {
-						'Content-Type': 'application/json',
-						'x-api-key': this.apiKey,
-						'x-service': this.service,
-						'x-namespace': this.namespace,
+						"Content-Type": "application/json",
+						"x-api-key": this.apiKey,
+						"x-service": this.service,
+						"x-namespace": this.namespace,
 					},
 					body: logsBody,
-				})
+				});
 				if (res.ok) {
 					// Remove the logs we sent
-					this.logs.splice(0, logsCount)
-					await res.arrayBuffer() // Read the body to completion
+					this.logs.splice(0, logsCount);
+					await res.arrayBuffer(); // Read the body to completion
 				} else {
-					console.log(`Baselime failed to ingest logs: ${res.status} ${res.statusText} ${await res.text()}`)
+					console.log(
+						`Baselime failed to ingest logs: ${res.status} ${
+							res.statusText
+						} ${await res.text()}`,
+					);
 				}
 			} catch (err) {
-				console.error(`Baselime failed to ingest logs: ${err}`)
+				console.error(`Baselime failed to ingest logs: ${err}`);
 			}
-		}
+		};
 
 		// Make sure the last one is done before starting a flush
-		await this.flushPromise
+		await this.flushPromise;
 
-		this.flushPromise = doFlush()
-		await this.flushPromise
-		this.flushPromise = null
+		this.flushPromise = doFlush();
+		await this.flushPromise;
+		this.flushPromise = null;
 	}
 
 	log(msg: string, data?: any) {
-		this._log(msg, 'info', data)
+		this._log(msg, "info", data);
 	}
 
 	info(msg: string, data?: any) {
-		this._log(msg, 'info', data)
+		this._log(msg, "info", data);
 	}
 
 	warn(msg: string, data?: any) {
-		this._log(msg, 'warning', data)
+		this._log(msg, "warning", data);
 	}
 
 	error(msg: string | Error | unknown, data?: any) {
-		let m = ''
+		let m = "";
 		if (msg instanceof Error) {
-			m = msg.message + (msg.stack ? `: ${msg.stack}` : '')
-		} else if (typeof msg === 'string') {
-			m = msg
+			m = msg.message + (msg.stack ? `: ${msg.stack}` : "");
+		} else if (typeof msg === "string") {
+			m = msg;
 		} else {
-			m = JSON.stringify(msg)
+			m = JSON.stringify(msg);
 		}
-		this._log(m, 'error', data)
+		this._log(m, "error", data);
 	}
 
 	debug(msg: string, data?: any) {
-		this._log(msg, 'debug', data)
+		this._log(msg, "debug", data);
 	}
 }
